585f98d8c800d3d837d5fa12433c10d6b5fcad36
oliver.t.poole <oliverpoole7@gmail.com>
2016-05-08 10:08:07 +0100

    Project reset

--- README.md
+++
@@ -1,3 +0,0 @@
-### Meet Dexter
-Dexter is a fury, four legged, music loving pooch who loves to help people build great playlists and discover new music on Spotify.
-
--- website/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-exports.login = function(req, res){
-  res.render('index', { title: 'Login' });
-};
